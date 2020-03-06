7fa285e24c8ed1df0f03b27f07edc6da03f9c237
megan o <mooi0004@student.monash.edu>
2020-03-06 10:42:16 +0800

    transition between fragments for bottom nav bar - done

---
+++ app/src/main/java/com/example/aavax/ui/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.aavax.ui;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.aavax.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        //I added this if statement to keep the selected fragment when rotating the device
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new HomePageFragment()).commit();
+        }
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.navigation_home:
+                            selectedFragment = new HomePageFragment();
+                            break;
+                        case R.id.navigation_travel:
+                            selectedFragment = new TravelPageFragment();
+                            break;
+                        case R.id.navigation_reminders:
+                            selectedFragment = new RemindersPageFragment();
+                            break;
+                        case R.id.navigation_profile:
+                            selectedFragment = new ProfilePageFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+}
--- app/src/main/java/com/example/aavax/ui/TravelPageFragment.java
+++ app/src/main/java/com/example/aavax/ui/TravelPageFragment.java
@@ -1,7 +1,9 @@
 package com.example.aavax.ui;
 
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,56 +13,11 @@ import android.view.ViewGroup;
 import com.example.aavax.R;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TravelPageFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class TravelPageFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public TravelPageFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TravelPage.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TravelPageFragment newInstance(String param1, String param2) {
-        TravelPageFragment fragment = new TravelPageFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_travel_page, container, false);
     }
 }
