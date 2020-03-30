4f4cfbcc2f6b4680d0c079e0584842f809a0ee15
lsscecilia <52391711+lsscecilia@users.noreply.github.com>
2020-03-30 20:02:02 +0800

    google maps part done

--- app/src/main/java/com/example/aavax/ui/TravelVaccinesFragment.java
+++ app/src/main/java/com/example/aavax/ui/TravelVaccinesFragment.java
@@ -1,12 +1,14 @@
 package com.example.aavax.ui;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -52,6 +54,17 @@ public class TravelVaccinesFragment extends Fragment {
         RecyclerViewAdapter adapterRec = new RecyclerViewAdapter(mRecommendedVaccines, mRecommendedTakenImgs);
         recyclerViewRecVaccines.setAdapter(adapterRec);
         recyclerViewRecVaccines.setLayoutManager(new LinearLayoutManager(getContext()));
+
+
+        final Button viewClinicBtn = view.findViewById(R.id.ViewClinicsBtn);
+        viewClinicBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
         return view;
     }
     @Override
